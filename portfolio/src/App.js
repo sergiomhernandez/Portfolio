befd9c9307042ba38cdb715b6350d4dfdbeed694
Sergio Hernandez <sergiomanueel@gmail.com>
2023-05-24 18:21:34 -0400

    Created portfolio project

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
