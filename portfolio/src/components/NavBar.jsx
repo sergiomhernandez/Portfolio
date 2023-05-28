4bcffb2a37b4cdb404600d080c70876bcb88d07f
Sergio Hernandez <sergiomanueel@gmail.com>
2023-05-27 23:56:15 -0400

    update

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -28,13 +28,13 @@ const NavBar = () => {
   return (
     <div name="navbar">
       <nav className='navbar'>
-        <div className="flex justify-center bg-transparent items-center w-full h-20 z-10 text-white fixed md:px-4 top-0 left-0">
-          <div className='flex justify-between md:max-w-6xl w-5/6'>
-              <a href='' className='text-xl tracking-[0.20em] ml-2 uppercase font-bold text- cursor-pointer'>Sergio</a>
+        <div className="flex justify-center bg-transparent items-center w-full h-20 z-10 text-white fixed md:px-4 top-0 left-0 bg-black">
+          <div className='flex justify-between md:max-w-6xl w-[90%] '>
+              <a href='' className='text-xl tracking-[0.20em] ml-2 uppercase font-bold list-none cursor-pointer'>Sergio</a>
             <ul className="hidden md:flex">
               {links.map(({id, link}) => (
                 <li key={id} 
-                className="px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 hover:text-cyan-200 duration-200"
+                className="px-4 list-item cursor-pointer capitalize font-medium text-white hover:scale-105 hover:text-cyan-200 duration-200"
                 >
                   <a href="#home"><Link to={link} smooth duration={500}>
                     {link}</Link></a>
